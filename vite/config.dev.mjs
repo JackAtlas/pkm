06dac35c4b60da4e9639e23a3555f954d135830e
JackAtlas <jackatlas8864@gmail.com>
2025-09-09 22:00:25 +0800

    chore(config): 设置路径别名

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: './',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
-        },
-    },
-    server: {
-        port: 8080
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser']
+        }
+      }
     }
-});
+  },
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+})
